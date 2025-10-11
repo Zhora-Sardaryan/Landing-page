0b61a0e19fecd883ad19293270aea4028a6f71e5
Zhora-77 <195808259+Zhora-77@users.noreply.github.com>
2025-10-12 00:29:23 +0400

    Add files via upload
    Toggle theme

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 const btnPopup = document.querySelector('.btn-login');
 const iconClose = document.querySelector('.icon-close');
+let darkmode = localStorage.getItem('darkmode');
+
+
 
 registerLink.addEventListener('click', ()=> {
   wrapper.classList.add('active');
@@ -18,4 +21,21 @@ btnPopup.addEventListener('click', ()=> {
 
 iconClose.addEventListener('click', ()=> {
   wrapper.classList.remove('active-popup');
+});
+
+registerLink.addEventListener('click', () => {
+  wrapper.classList.add('active');
+})
+
+const toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', function() {
+  document.body.classList.toggle('darkmode');
+  if(document.body.classList.contains('darkmode')) {
+    toggle.querySelector('svg:last-child').style.display = 'none';
+    toggle.querySelector('svg:first-child').style.display = 'block';
+  } else {
+    toggle.querySelector('svg:first-child').style.display = 'none';
+    toggle.querySelector('svg:last-child').style.display = 'block';
+  }
 });
